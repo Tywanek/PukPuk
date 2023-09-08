10aea05cf3d6f1d30167dfe1ca4852d666029037
Radoslaw Tywanek <radoslaw.tywanek@knowit.eu>
2023-09-08 02:07:44 +0200

    new icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,8 +76,13 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
 
-    testImplementation("io.mockk:mockk:1.12.0")
-        testImplementation( "org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.2")
+    testImplementation("org.mockito:mockito-core:3.10.0") 
+    testImplementation("org.mockito:mockito-inline:3.10.0")
+
+
+
+
+
 
 
 }
--- app/src/main/java/com/radlab/pukpuk/MainActivity.kt
+++ app/src/main/java/com/radlab/pukpuk/MainActivity.kt
@@ -3,7 +3,6 @@ package com.radlab.pukpuk
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -21,18 +20,18 @@ import androidx.compose.ui.unit.dp
 import com.radlab.pukpuk.repositories.RandomJokeRepository
 import com.radlab.pukpuk.ui.theme.PukPukTheme
 import com.radlab.pukpuk.viewmodels.JokeFlowViewModel
-import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PukPukTheme {
+                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SlowlyAppearingText(viewModel = JokeFlowViewModel(RandomJokeRepository()))
+                    RandomJokeScreen(viewModel = JokeFlowViewModel(RandomJokeRepository()))
                 }
             }
         }
@@ -40,33 +39,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun SlowlyAppearingText(viewModel: JokeFlowViewModel, durationMillis: Int = 2000) {
+fun RandomJokeScreen(viewModel: JokeFlowViewModel) {
     var text by remember { mutableStateOf("") }
-    var visibleText by remember { mutableStateOf("") }
-
     LaunchedEffect(Unit) {
         viewModel.getRandomJoke().collect { joke ->
             text = joke
-            for (i in joke.indices) {
-                visibleText = joke.substring(0, i + 1)
-                delay(durationMillis.toLong() / joke.length)
-            }
         }
     }
+    Text(text = text, modifier = Modifier.padding(16.dp))
+}
 
-    Column {
-        Text(
-            text = visibleText, modifier = Modifier
-                .padding(25.dp)
-                .padding(top = 36.dp)
-        )
-    }
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PukPukTheme {
-        SlowlyAppearingText(viewModel = JokeFlowViewModel(RandomJokeRepository()))
+        Greeting("Android")
     }
 }
